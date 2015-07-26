b00cc3ea2febb015fd218a7a6fa826cd8c8027a9
shayan <saaiaic4@gmail.com>
2015-07-26 17:43:45 +0800

    can jump.

---
+++ canJump.py
@@ -0,0 +1,30 @@
+class Solution:
+    # @param {integer[]} nums
+    # @return {boolean}
+    def canJump(self, nums):
+        n = len(nums)
+        if n == 0 or n == 1:
+            return True
+        last = n-1
+        i = n-2
+        while(i >= 0 ):
+            if (i + nums[i])>=last:
+                last = i
+            i -= 1
+        return last <= 0
+    # @param {integer[]} nums
+    # @return {boolean}
+    def myCanJump(self, nums):
+        n = len(nums)
+        if n == 0 or n == 1:
+            return True
+        i = 0
+        max_jump = nums[i]
+        while(i < n):
+            max_jump = max(nums[i],max_jump)
+            if max_jump == 0:
+                break
+            else:
+                i+=1 
+                max_jump -= 1
+        return i >= n-1    
