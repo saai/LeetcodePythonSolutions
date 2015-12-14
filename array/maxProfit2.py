2207f960f8f5e3d90a8babdda2b5d24c54e8bd8c
shayan <saaiaic4@gmail.com>
2015-12-14 23:28:07 +0800

    added max profit for 2 transaction

--- array/maxProfit2.py
+++ array/maxProfit2.py
@@ -18,3 +18,22 @@ class Solution(object):
             if i>0 and p>prices[i-1]:      
                 profits = max((profits+p-prices[i-1]),p-min_v)
         return profits
+
+    # get every profits you can get.
+    def maxProfit2(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_p = 0
+        i  = 0 
+        while(i < len(prices)):
+            while(i+1< len(prices) and prices[i] >= prices[i+1]):
+                i += 1
+            low = prices[i]
+            while(i+1 < len(prices) and prices[i]<=prices[i+1]):
+                i += 1
+            high = prices[i]
+            max_p += high - low
+            i += 1
+        return max_p     
